58daf69738608b316d70f40b5c9424d46e629782
Brenden Ehlers <brenden@theehlers.org>
2024-02-18 09:28:08 -0700

    updated client, added tests

--- quiet_hn/hn/client.go
+++ quiet_hn/hn/client.go
@@ -1,4 +1,4 @@
-package client
+package hn
 
 import (
 	"encoding/json"
--- quiet_hn/main.go
+++ quiet_hn/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"gophercises/quiet_hn/client"
+	"gophercises/quiet_hn/hn"
 )
 
 func main() {
-	var c client.Client
+	var c hn.Client
 
 	item, err := c.GetItem(39420256)
 	if err != nil {
