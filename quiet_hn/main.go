21ba3437c1faf20c63ef20cddf5721057b9e17a8
Brenden Ehlers <brenden@theehlers.org>
2024-02-18 11:20:56 -0700

    updated main

--- quiet_hn/main.go
+++ quiet_hn/main.go
@@ -28,11 +28,9 @@ func check(err error) {
 
 func serveTemplate(w http.ResponseWriter, r *http.Request) {
 	lp := filepath.Join("templates", "layout.html")
+	tmpl := template.Must(template.ParseFiles(lp))
 
 	items, err := hn.TopItems(30)
-	check(err)
-
-	tmpl, err := template.ParseFiles(lp)
 	check(err)
 
 	err = tmpl.ExecuteTemplate(w, "layout", items)
