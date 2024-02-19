eff5127eea8a06b8d33b989d5757ded6e71dc90b
Brenden Ehlers <brenden@theehlers.org>
2024-02-19 12:16:20 -0700

    finished challenge

--- recover/main.go
+++ recover/main.go
@@ -4,14 +4,68 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"runtime/debug"
 )
+
+type responseWriter struct {
+	http.ResponseWriter
+	writes [][]byte
+	status int
+}
+
+func (rw *responseWriter) Write(bytes []byte) (int, error) {
+	rw.writes = append(rw.writes, bytes)
+	return len(bytes), nil
+}
+
+func (rw *responseWriter) WriteHeader(status int) {
+	rw.status = status
+}
+
+func (rw *responseWriter) flush() error {
+	if rw.status != 0 {
+		rw.ResponseWriter.WriteHeader(rw.status)
+	}
+
+	for _, write := range rw.writes {
+		_, err := rw.ResponseWriter.Write(write)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/panic/", panicDemo)
 	mux.HandleFunc("/panic-after/", panicAfterDemo)
 	mux.HandleFunc("/", hello)
-	log.Fatal(http.ListenAndServe(":3000", mux))
+
+	log.Println("Hosted server on localhost:3000")
+	log.Fatal(http.ListenAndServe(":3000", recoverMW(mux, true)))
+}
+
+func recoverMW(app http.Handler, dev bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Println("Recovered error:", r)
+
+				if dev {
+					log.Println("Panic stack trace:")
+					stack := string(debug.Stack())
+					log.Println(stack)
+				}
+
+				http.Error(w, "Something went wrong", http.StatusInternalServerError)
+			}
+		}()
+
+		nw := &responseWriter{ResponseWriter: w}
+		app.ServeHTTP(nw, r)
+		nw.flush()
+	}
 }
 
 func panicDemo(w http.ResponseWriter, r *http.Request) {
